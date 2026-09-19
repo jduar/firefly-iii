65b5103377ea8f1e1f28f36aaa28eeebe1658702
João Duarte <jduar.dev@proton.me>
2026-09-19 01:48:33 +0100

    forgot language file

---
+++ resources/lang/en_US/transaction_templates.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+// Kept out of firefly.php to facilitate upstream merges to fork given the feature likely won't be upstreamed.
+return [
+    'title'                         => 'Transaction templates',
+    'breadcrumb'                    => 'Transaction templates',
+    'breadcrumb_create'             => 'Create a new template',
+    'breadcrumb_edit'               => 'Edit template',
+    'breadcrumb_delete'             => 'Delete template',
+    'create_new'                    => 'Create a new template',
+    'edit_title'                    => 'Edit template',
+    'delete_title'                  => 'Delete template',
+    'delete_confirm'                => 'Are you sure you want to delete the template ":name"? Transactions created from it are not affected.',
+    'none_yet'                      => 'You have no transaction templates yet.',
+    'no_value'                      => '(none)',
+    'stored'                        => 'Template stored.',
+    'updated'                       => 'Template updated.',
+    'deleted'                       => 'Template deleted.',
+    'picker_placeholder'            => 'Fill from template...',
+    'field_name'                    => 'Template name',
+    'field_transaction_description' => 'Transaction description',
+    'field_source'                  => 'Source account',
+    'field_destination'             => 'Destination account',
+    'field_budget'                  => 'Budget',
+    'field_category'                => 'Category',
+    'field_tags'                    => 'Tags',
+    'field_notes'                   => 'Notes',
+    'help_transaction_description'  => 'Fills the description field of the new transaction.',
+    'help_category'                 => 'Only existing categories can be selected. Create the category first if it does not exist yet.',
+    'help_tags'                     => 'Separate tags with commas.',
+];
